2306aa7871ce1685c4e5a6efdbb1a730cae6a8e8
Hunor Csomortáni <csomh@redhat.com>
2017-06-26 10:34:33 +0200

    Change /usr ot /run/httpd

--- docker-setup.sh
+++ docker-setup.sh
@@ -4,12 +4,12 @@
 mkdir -p /data
 chown -R httpd:0 /data
 chmod g+w -R /data
-chown -R httpd:0 /usr
+chown -R httpd:0 /run/httpd
 chown -R httpd:0 /var
 
-chgrp -R 0 /usr
-chmod -R g+rw /usr
-find /usr -type d -exec chmod g+x {} +
+chgrp -R 0 /run/httpd
+chmod -R g+rw /run/httpd
+find /run/httpd -type d -exec chmod g+x {} +
 
 chgrp -R 0 /var
 chmod -R g+rw /var
